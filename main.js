39b25349f6052767ea682bc76466c9b72d50c34a
Marion Séminel <36150560+snugglejuice@users.noreply.github.com>
2019-06-07 17:14:26 +0200

    add button animation
    - fluid round button
    - translation button apparition

---
+++ main.js
@@ -0,0 +1,54 @@
+const socialBtn = document.getElementById('social');
+const contactBtn = document.getElementById('contact');
+const hexagonBtn = document.getElementById('hexagon');
+
+
+
+const displaySocialButtons = () => {
+
+  let social = document.getElementById('social');
+  let facebook = document.getElementById('facebook');
+  let internet = document.getElementById('internet');
+  let linkedin = document.getElementById('linkedin');
+
+  if (social.className === 'button') {
+
+    facebook.className += ' facebook';
+    internet.className += ' internet';
+    linkedin.className += ' linkedin';
+    social.className += ' clicked';
+
+  }
+
+  else {
+    facebook.className = 'button';
+    internet.className = 'button';
+    linkedin.className = 'button';
+    social.className = 'button';
+  }
+
+}
+
+const displayContactButtons = () => {
+  let mail = document.getElementById('mail');
+  let call = document.getElementById('call');
+
+  if (mail.className === 'contactButton contactButton--white') {
+    mail.className += ' mail';
+    call.className += ' call';
+  }
+  else {
+    mail.className = 'contactButton contactButton--white';
+    call.className = 'contactButton contactButton--white';
+  }
+
+}
+
+const flip = () => {
+  let hexagonBtn = document.getElementById('hexagon');
+  hexagonBtn.className += ' flip';
+}
+
+socialBtn.onclick = displaySocialButtons;
+contactBtn.onclick = displayContactButtons;
+hexagonBtn.onclick = flip;
